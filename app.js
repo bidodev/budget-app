0109a04b6be165f2b91445fc19bb8a4cb193cdd9
Claudinei Bido <bido.c@live.com>
2020-04-09 11:08:24 +0200

    income and expense classes

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-/** Budget Controller
- *
- */
+//Budget Controller
 const budgetController = (function () {
-  //do something
+  class Expense {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  class Income {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
 })();
 
 /** UI Controller
