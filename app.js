8b8f66e713df7c98ee1d65e58dcb87ddbf045b27
Claudinei Bido <bido.c@live.com>
2020-04-09 10:36:05 +0200

    Reading input Data

--- app.js
+++ app.js
@@ -9,33 +9,54 @@ const budgetController = (function () {
  *
  */
 const userInterfaceController = (function () {
-  //do something
+  const DOMstrings = {
+    inputType: ".add__type",
+    inputDesc: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn",
+  };
+
+  return {
+    getInput() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDesc).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings() {
+      return DOMstrings;
+    },
+  };
 })();
 
 /** Global App Controller
  *
  */
 const appController = (function (budgetCtrl, UICrtl) {
-  //grab the button
-  const addBtn = document.querySelector(".add__btn");
+  const DOM = UICrtl.getDOMstrings();
 
-  //add the event listener
-  addBtn.addEventListener("click", () => {
-    /** As soon someone hit the button we have to do the follow steps.
-     *
-     * Get the filed input data
-     * Add the item to the budget controller
-     * Add the new item to the User Interface
-     * Calculate the budget
-     * Display the budget on the User Interface
-     *
-     */
-  });
+  const controllerAddItem = () => {
+    //1. Get the filed input data
+    const input = UICrtl.getInput();
+    console.log(input);
+
+    //2. Add the item to the budget controller
+    //Add the new item to the User Interface
+    //Calculate the budget
+    //Display the budget on the User Interface
+  };
+
+  //grab the button
+  document
+    .querySelector(DOM.inputBtn)
+    .addEventListener("click", controllerAddItem);
 
   document.addEventListener("keypress", (event) => {
     //check if the user pressed the return key (enter)
     if (event.keyCode === 13) {
-      //do something when enter is pressed
+      controllerAddItem();
     }
   });
 })(budgetController, userInterfaceController);
